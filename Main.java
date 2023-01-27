a0f607863f853fba41876a20854056d442e78e26
Rupesh-Gaur2001 <90850733+Rupesh-Gaur2001@users.noreply.github.com>
2023-01-27 22:22:09 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,110 +1,30 @@
-public class Main implements olaowner,oladriver,olaCustomer{
+public class Main {
     public static void main(String[] args) {
-       olaCustomer sc=new Main();
-       olaowner b=new Main();
-       oladriver c=new Main();
-
-    }
-
-    @Override
-    public void signup() {
-
-    }
-
-    @Override
-    public void cancelride() {
-
-    }
-
-    @Override
-    public void searchride() {
-
-    }
-
-    @Override
-    public void choosepickup() {
-
-    }
-
-    @Override
-    public void choosedrop() {
-
-    }
-
-    @Override
-    public void acceptaride() {
-
-    }
-
-    @Override
-    public void cancelaaride() {
-
-    }
-
-    @Override
-    public void seeroute() {
-
-    }
-
-    @Override
-    public void transfermoneytowallet() {
-
-    }
-
-    @Override
-    public void myearings() {
-
-    }
-
-    @Override
-    public void carservicebooking() {
-
-    }
-
-    @Override
-    public void getcustomerlocation() {
-
-    }
-
-    @Override
-    public void login() {
-
-    }
-
-    @Override
-    public void logout() {
-
-    }
-
-    @Override
-    public void getdriverinfo() {
-
+        Amazon b=new Amazon();
+       System.out.println(b.totalsum(1000));
     }
 }
-public interface olaCustomer{
-    void signup();
-    void logout();
-    void cancelride();
-    void searchride();
-    void choosepickup();
-    void choosedrop();
+
+class Amazon{
+   gpay a=new gpay();
+
+    public float totalsum(int amt){
+        float totalsum=amt+(amt*a.getTxncharge());
+        return totalsum;
+    }
 
 }
-public interface oladriver{
-    void acceptaride();
-    void cancelaaride();
-    void seeroute();
-    void transfermoneytowallet();
-    void myearings();
-    void carservicebooking();
-    void login();
-    void logout();
-    void getcustomerlocation();
-}
-public interface olaowner{
-    void login();
-    void logout();
-    void getdriverinfo();
+class gpay{
+   private float txncharge=0.05f;
+    public float getTxncharge(){
+        return txncharge;
+    }
+    public void Set(float newcharge){
+        if(newcharge<0.5f){
+            System.out.println("Invalid charge");
 
-
+        }else{
+            txncharge=newcharge;
+        }
+    }
 }
